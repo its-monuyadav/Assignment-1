74c34a6172d28b5bcb50b94610962f1270c74b32
Monu <kumarmonu1812@gmail.com>
2022-09-21 22:35:25 +0530

    https://github.com/its-monuyadav/DSA-.git

---
+++ input_output.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    
+    int a;
+    cin>>a;
+     int b;
+     cin>>b;
+
+    int sum = a+b;
+    cout << sum;  
+    return 0;
+}
